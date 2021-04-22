544d1825bcc128f9249159487371b9683551f983
Igor-HBS <70449472+Igor-HBS@users.noreply.github.com>
2021-04-22 13:16:31 -0300

    Add files via upload

---
+++ lab04/src/mc322/lab04/Peca.java
@@ -0,0 +1,10 @@
+package mc322.lab04;
+
+public class Peca
+{
+    // Retorna um caractere que representa a peça;
+    public char apresenta()
+    {
+        return 'P';
+    }
+}
---
+++ lab04/src/mc322/lab04/Tabuleiro.java
@@ -0,0 +1,156 @@
+package mc322.lab04;
+
+public class Tabuleiro
+{
+    Peca posicoes[][];
+    int num_pecas;
+    /* Descreve a forma do tabuleiro:
+    1: espaço válido
+    0: espaço inválido
+    */
+    private static int formaTabuleiro[][] = {
+        {0, 0, 1, 1, 1, 0, 0},
+        {0, 0, 1, 1, 1, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1},
+        {0, 0, 1, 1, 1, 0, 0},
+        {0, 0, 1, 1, 1, 0, 0},
+    };
+
+    // Inicia um novo tabuleiro completo;
+    Tabuleiro()
+    {
+        posicoes = new Peca[7][7];
+        for (int i = 0; i < 7; i++)
+            for(int j = 0; j < 7; j++)
+            {
+                if (!ehEspacoValido(i, j) || (i == 3 && j == 3))
+                {
+                    posicoes[i][j] = null;
+                }
+                else
+                {
+                    posicoes[i][j] = new Peca();
+                }
+            }
+        num_pecas =  32;
+    }
+
+    // Checa se o espaço informado é válido
+    private boolean ehEspacoValido(int i, int j)
+    {
+        if (i < 0 || i > 6 || j < 0 || j > 6)
+        {
+            return false;
+        }
+        if (formaTabuleiro[i][j] == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // Checa se o movimento é válido
+    private boolean ehMovimentoValido(int iInicio, int jInicio, int iFim, int jFim)
+    {
+        if(ehEspacoValido(iInicio, jInicio) && posicoes[iInicio][jInicio] != null)
+            if (ehEspacoValido(iFim, jFim) && posicoes[iFim][jFim] == null)
+            {
+                // movimento é horizontal
+                if(iInicio == iFim && (jInicio - jFim == 2 || jInicio - jFim == -2))
+                {
+                    if(posicoes[iInicio][(jInicio + jFim) / 2] != null)
+                    {
+                        return true;
+                    }
+                }
+                // movimento é vertical
+                else if(jInicio == jFim && (iInicio - iFim == 2 || iInicio - iFim == -2))
+                {
+                    if(posicoes[(iInicio + iFim) / 2][jInicio] != null)
+                    {
+                        return true;
+                    }
+
+                }
+            }
+        // inválido
+        return false;
+    }
+
+    // Realiza o movimento indicado, quando possível. Caso contrário, não faz nada.
+    public void mover(int iInicio, int jInicio, int iFim, int jFim)
+    {
+        if (ehMovimentoValido(iInicio, jInicio, iFim, jFim))
+        {
+            // realiza o salto:
+            posicoes[iFim][jFim] = posicoes[iInicio][jInicio];
+            posicoes[iInicio][jInicio] = null;
+            // Come a peça;
+            if (iInicio == iFim)
+            {
+                posicoes[iInicio][(jInicio + jFim) / 2] = null;
+            }
+            else if (jInicio == jFim)
+            {
+                posicoes[(iInicio + iFim) / 2][jInicio] = null;
+            }
+            num_pecas = num_pecas - 1;
+        }
+    }
+
+    // Retorna uma string que representa a linha
+    private String stringLinha(int i)
+    {
+        String res = "";
+
+        for(int j = 0; j < 7; j++)
+        {
+            if(ehEspacoValido(i, j))
+            {
+                if(posicoes[i][j] != null)
+                {
+                    res += posicoes[i][j].apresenta();
+                }
+                else
+                {
+                    res += '-';
+                }
+            }
+            else
+            {
+                res += ' ';
+            }
+        }
+
+        return res + "\n";
+    }
+    // Apresenta o estado atual do tabuleiro;
+    public void apresenta()
+    {
+        for(int i = 0; i < 7; i++)
+        {
+            System.out.printf("%d ", i + 1);
+            System.out.print(stringLinha(i));
+        }
+        System.out.println("  abcdefg");
+        System.out.println();
+    }
+
+    public String toString()
+    {
+        String res = "";
+
+        for(int i = 0; i < 7; i++)
+        {
+            res += stringLinha(i);
+        }
+
+        return res;
+    }
+
+}
